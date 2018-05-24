e795c95d3121031dba312311d874ed9c052537c2
Giorvi Alvarez <giorvialvarez@Giorvis-MacBook-Pro.local>
2018-05-24 09:12:50 -0700

    the Bees move when certain keys are pressed

--- animation.js
+++ animation.js
@@ -8,28 +8,62 @@ function myFunction() {
         x.style.display = "none";
     }
 }
+
 document.addEventListener("click", myFunction);
 
 
-// Box Object definition
-function Box(xpos, ypos, xstep, ystep, color, id) {
+function Box(xpos, ypos, xstep, ystep, id) {
     this.xpos = xpos;
     this.ypos = ypos;
     this.xstep = xstep;
     this.ystep = ystep;
     this.id = id;
-    this.color = color;
     var elem = document.getElementById(this.id);
     this.render = function () {
         elem.style.top = this.ypos + 'px';
         elem.style.left = this.xpos + 'px';
-        //elem.style.backgroundColor = color;
         elem.style.backgroundImage = "url('beee.png')";
         elem.style.backgroundSize = "50px 50px";
     }
+    this.changeDirection = function (direction) {
+
+        if (direction === "up") {
+            this.ystep = -2;
+            this.xstep = 0;
+        }
+        if (direction === "down") {
+            this.ystep = 2;
+            this.xstep = 0;
+        }
+        if (direction === "right") {
+            this.xstep = 2;
+            this.ystep = 0;
+        }
+        if (direction === "left") {
+            this.xstep = -2;
+            this.ystep = 0;
+        }
+        if (direction === "upright") {
+            this.xstep = -2;
+            this.ystep = 2;
+        }
+        if (direction === "upleft") {
+            this.xstep = -2;
+            this.ystep = -2;
+        }
+        if (direction === "downright") {
+            this.xstep = -2;
+            this.ystep = 2;
+        }
+        if (direction === "upright") {
+            this.xstep = -2;
+            this.ystep = 2;
+        }
+
+    }
 }
 
-// Some variables that we need below
+
 var boxes = [];
 var numBoxes = 50;
 var container = document.getElementById("boxContainer");
@@ -40,19 +74,20 @@ for (var i = 0; i < numBoxes; i++) {
     elem.className = "box";
     elem.id = "box" + i;
     container.appendChild(elem);
-    boxes[i] = new Box(Math.random() * 350,
-        Math.random() * 350,
-        Math.random(),
-        Math.random(),
-        '#' + Math.floor(Math.random() * 16777215).toString(16),
-        elem.id);
+    boxes[i] = new Box(Math.random() * 350, Math.random() * 350, 0, 0, elem.id);
 }
 
 var id = setInterval(frame, 5);
 
-// The animation code
+
 function frame() {
-    // Box 1
+
+    collideBorder();
+}
+
+
+function collideBorder() {
+
     for (var i = 0; i < boxes.length; i++) {
         if (boxes[i].xpos > 450 || boxes[i].xpos < 0) {
             boxes[i].xstep = -boxes[i].xstep;
@@ -64,5 +99,38 @@ function frame() {
         boxes[i].xpos = boxes[i].xpos + boxes[i].xstep;
         boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
         boxes[i].render();
+    }
+}
+window.addEventListener("keypress", movesnake);
+
+
+function movesnake(event) {
+
+    for (var i = 0; i < boxes.length; i++) {
+
+        if (event.key === "w") {
+            boxes[i].changeDirection("up");
+        }
+        if (event.key === "s") {
+            boxes[i].changeDirection("down");
+        }
+        if (event.key === "a") {
+            boxes[i].changeDirection("left");
+        }
+        if (event.key === "d") {
+            boxes[i].changeDirection("right");
+        }
+        if (event.key === "q") {
+            boxes[i].changeDirection("upleft");
+        }
+        if (event.key === "e") {
+            boxes[i].changeDirection("upright");
+        }
+        if (event.key === "z") {
+            boxes[i].changeDirection("downleft");
+        }
+        if (event.key === "c") {
+            boxes[i].changeDirection("downright");
+        }
     }
 }
